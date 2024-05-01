94852d0b8bb2993774b230fd23d7a9983a7cb1f6
amitwebinopoly <amit@webinopoly.com>
2024-05-01 22:28:53 +0530

    backup-01-05-2024

--- web/app/Http/Controllers/FrontEndController.php
+++ web/app/Http/Controllers/FrontEndController.php
@@ -37,10 +37,37 @@ class FrontEndController extends Controller {
 	public function get_cust_point(){
 		if(isset($_POST['email']) && !empty($_POST['email']) ){
 			$GMController = new GMController();
+
+			$shop = $_POST['shop'];
+			$namespace = 'gm_rewards';
+			$key = 'settings';
+
 			$cw_data = $GMController->get_member_id_from_email($_POST['email']);
 			if(isset($cw_data['MemberNumber']) && !empty($cw_data['MemberNumber'])){
 				$wallet_data = $GMController->get_member_data_from_member_id($cw_data['MemberNumber']);
 				if(isset($wallet_data)){
+					$shop_db_data = \App\Models\Session::where('shop', $shop)->get()->toArray();
+
+					$points_exchange = 1;
+					if(isset($shop_db_data) && !empty($shop_db_data)) {
+						$token = $shop_db_data[0]['access_token'];
+						$headers = array(
+							'X-Shopify-Access-Token' => $token
+							//'X-Shopify-Storefront-Access-Token' => $storefront_access_token
+						);
+						$GraphqlController = new GraphqlController($shop, $headers, false); //pass true for store front apis
+
+						//check metafield exist or not
+						$query = '{ shop { id metafield(namespace:"'.$namespace.'",key:"'.$key.'"){ id value } } }';
+						$shop_data = $GraphqlController->runByQuery($query);
+						if(isset($shop_data['data']['shop']['metafield']['value']) && !empty($shop_data['data']['shop']['metafield']['value']) ){
+							$arr = json_decode($shop_data['data']['shop']['metafield']['value'],1);
+							if(isset($arr['points_exchange']) && !empty($arr['points_exchange'])){
+								$points_exchange = $arr['points_exchange'];
+							}
+						}
+					}
+
 					$res['success'] = 'true';
 					$res['message'] = @$wallet_data['RespMessage'];
 					$res['data'] = [
@@ -48,6 +75,7 @@ class FrontEndController extends Controller {
 						'total_points' => @$wallet_data['LOY Member']['Total Points'],
 						'next_tier' => @$wallet_data['LOY Member']['Tier']['Next Tier Name'],
 						'current_tier' => @$wallet_data['LOY Member']['Tier']['Current Tier Name'],
+						'points_exchange' => $points_exchange
 					];
 				}else{
 					$res['success'] = 'false';
@@ -68,10 +96,12 @@ class FrontEndController extends Controller {
 		$GMController = new GMController();
 		$InexController = new InexController();
 		$shop = $_POST['shop'];
+		$namespace = 'gm_rewards';
+		$key = 'settings';
 
 		$validator = Validator::make($request->all(), [
 			'email' => 'required|email',
-			'amount' => 'required',
+			'points' => 'required',
 			'customer_id' => 'required',
 			'variant_ids' => 'required',
 		]);
@@ -87,66 +117,90 @@ class FrontEndController extends Controller {
 				);
 				$GraphqlController = new GraphqlController($shop, $headers, false); //pass true for store front apis
 
-				//$cw_data = $GMController->get_member_id_from_email($_POST['email']);
-				//if(isset($cw_data['MemberNumber']) && !empty($cw_data['MemberNumber'])){
-
-					$discount_code = 'GMREWARD_'.$InexController->INEX_random_string();
-					$variant_ids_arr = explode(',',$_POST['variant_ids']);
-					$productVariantsToAddArr = [];
-					foreach($variant_ids_arr as $var_id){
-						$productVariantsToAddArr[] = '"gid://shopify/ProductVariant/'.$var_id.'"';
+				//check metafield exist or not
+				$query = '{ shop { id metafield(namespace:"'.$namespace.'",key:"'.$key.'"){ id value } } }';
+				$shop_data = $GraphqlController->runByQuery($query);
+				$points_exchange = 1;
+				if(isset($shop_data['data']['shop']['metafield']['value']) && !empty($shop_data['data']['shop']['metafield']['value']) ){
+					$arr = json_decode($shop_data['data']['shop']['metafield']['value'],1);
+					if(isset($arr['points_exchange']) && !empty($arr['points_exchange'])){
+						$points_exchange = $arr['points_exchange'];
 					}
+				}
 
-					//date_default_timezone_set('America/New_York');
-					$startsAt = date("Y-m-d\TH:i:s\Z");
-					$endsAt = date("Y-m-d\TH:i:s\Z", strtotime($startsAt) + 3600);
+				$cw_data = $GMController->get_member_id_from_email($_POST['email']);
+				if(isset($cw_data['MemberNumber']) && !empty($cw_data['MemberNumber'])){
+					$wallet_data = $GMController->get_member_data_from_member_id($cw_data['MemberNumber']);
 
-					//create discount code
-					$mutation = 'mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) {
-					  discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) {
-						codeDiscountNode { id }
-						userErrors { field message }
-					  }
-					}';
-					$input = '{
-					  "basicCodeDiscount": {
-						"appliesOncePerCustomer": true,
-						"code": "'.$discount_code.'",
-						"combinesWith": { "orderDiscounts": true },
-						"customerGets": {
-						  "items": {
-							"products": {
-							  "productVariantsToAdd": [ '.(implode(',',$productVariantsToAddArr)).' ]
+					if(isset($wallet_data)){
+						$total_available_points = @$wallet_data['LOY Member']['Total Points'];
+						if($total_available_points >= $_POST['points']){
+							$discount_amount = floatval($_POST['points'])/floatval($points_exchange);
+							$discount_code = 'GMREWARD_'.$InexController->INEX_random_string();
+							$variant_ids_arr = explode(',',$_POST['variant_ids']);
+							$productVariantsToAddArr = [];
+							foreach($variant_ids_arr as $var_id){
+								$productVariantsToAddArr[] = '"gid://shopify/ProductVariant/'.$var_id.'"';
 							}
-						  },
-						  "value": { "discountAmount": { "amount": "'.bcdiv($_POST['amount'],1,2).'", "appliesOnEachItem": true } }
-						},
-						"customerSelection": {
-						  "customers": { "add": [ "gid://shopify/Customer/'.$_POST['customer_id'].'" ] }
-						},
-						"startsAt": "'.$startsAt.'",
-						"endsAt": "'.$endsAt.'",
-						"title": "GM Reward Discount",
-						"usageLimit": 1
-					  }
-					}';
 
-					$dcRes = $GraphqlController->runByMutation($mutation,$input);
-					if($dcRes['data']['discountCodeBasicCreate']['codeDiscountNode']['id']){
-						$res['success'] = 'true';
-						$res['message'] = '';
-						$res['data'] = [
-							'id' => $dcRes['data']['discountCodeBasicCreate']['codeDiscountNode']['id'],
-							'discount_code' => $discount_code
-						];
+							//date_default_timezone_set('America/New_York');
+							$startsAt = date("Y-m-d\TH:i:s\Z");
+							$endsAt = date("Y-m-d\TH:i:s\Z", strtotime($startsAt) + 3600);
+
+							//create discount code
+							$mutation = 'mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) {
+							  discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) {
+								codeDiscountNode { id }
+								userErrors { field message }
+							  }
+							}';
+							$input = '{
+							  "basicCodeDiscount": {
+								"appliesOncePerCustomer": true,
+								"code": "'.$discount_code.'",
+								"combinesWith": { "orderDiscounts": true },
+								"customerGets": {
+								  "items": {
+									"products": {
+									  "productVariantsToAdd": [ '.(implode(',',$productVariantsToAddArr)).' ]
+									}
+								  },
+								  "value": { "discountAmount": { "amount": "'.bcdiv($discount_amount,1,2).'", "appliesOnEachItem": true } }
+								},
+								"customerSelection": {
+								  "customers": { "add": [ "gid://shopify/Customer/'.$_POST['customer_id'].'" ] }
+								},
+								"startsAt": "'.$startsAt.'",
+								"endsAt": "'.$endsAt.'",
+								"title": "GM Reward Discount",
+								"usageLimit": 1
+							  }
+							}';
+
+							$dcRes = $GraphqlController->runByMutation($mutation,$input);
+							if($dcRes['data']['discountCodeBasicCreate']['codeDiscountNode']['id']){
+								$res['success'] = 'true';
+								$res['message'] = '';
+								$res['data'] = [
+									'id' => $dcRes['data']['discountCodeBasicCreate']['codeDiscountNode']['id'],
+									'discount_code' => $discount_code
+								];
+							}else{
+								$res['success'] = 'false';
+								$res['message'] = 'Something goes wrong while creating discount code.';
+							}
+						}else{
+							$res['success'] = 'false';
+							$res['message'] = 'You have not sufficient points';
+						}
 					}else{
 						$res['success'] = 'false';
-						$res['message'] = 'Something goes wrong while creating discount code.';
+						$res['message'] = @$wallet_data['RespMessage'];
 					}
-				/*}else{
+				}else{
 					$res['success'] = 'false';
 					$res['message'] = @$cw_data['RespMessage'];
-				}*/
+				}
 			}
 			else{
 				$res['success'] = 'false';
